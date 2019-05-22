57abb19eeb254413a7598c2336f3964f5fd2d67c
Céline Cossez <celine.cossez@coding-academy.fr>
2019-05-22 15:15:00 +0200

    add Home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import login from "./components/Home/login";
 import register from './components/Home/register';
+import home from "./components/Home/home";
 
 
 function App() {
   return (
     <Router>
+        <Route path="/" component={home}/>
         <Route path="/login" component={login}/>
         <Route path="/register" component={register}/>
     </Router>
