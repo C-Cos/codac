f73a5b244d8c4e3b5e730c510db430a0049c7e73
Elodie <elodie.szwarc@coding-academy.fr>
2019-05-22 16:55:44 +0200

    Register updated with zipcode

---
+++ frontend/src/components/Home/Zipinput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class Zipinput extends React.Component {
+
+    render () {
+        return (
+            <input type="text" maxLength="5" value={this.props.value} className="form-control" onChange={this.props.onChangeZipcode} />
+
+        )
+      }
+}
