eaefdc8e189a9f100d9bbb2e53cb88b5ea5a79cc
Fabien <fabien.oren@coding-academy.fr>
2019-05-29 17:43:52 +0200

    mocha test cleaning

--- backend/test/mocha.users.js
+++ backend/test/mocha.users.js
@@ -1,36 +1,34 @@
 process.env.NODE_ENV = 'test';
-let server = require('../index');
-let chai = require('chai');
-let chaiHttp = require('chai-http');
+const server = require('../index');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 chai.should();
 
 describe("Users", () => {
-        // it("should register a user", (done) => {
-        //     let user = {
-        //         username: "Testeur",
-        //         email: "test@test.com",
-        //         password: "test",
-        //         postcode: "75000",
-        //         city: "PARIS",
-        //         image: "test",
-        //         creation_date: new Date(),
-        //         edition_date: new Date(),
-        //         admin: false,
-        //         association: false
-        //     }
-        //      chai.request(users)
-        //          .post('/users/register')
-        //          .send(user)
-        //          .end((err, res) => {
-        //              res.should.have.status(200);
-        //              //res.body.should.be.a('object');
-        //              done();
-        //           });
-        //  });
+        it("should register a user", (done) => {
+            let user = {
+                username: "Testeur",
+                email: "test@test.com",
+                password: "test",
+                postcode: "75000",
+                city: "PARIS",
+                image: "test",
+                creation_date: new Date(),
+                edition_date: new Date(),
+                admin: false,
+                association: false
+            }
+             chai.request(server)
+                 .post('/users/register')
+                 .send(user)
+                 .end((err, res) => {
+                     res.should.have.status(200);
+                     done();
+                  });
+         });
 
-        // Test to get single student record
         it("should get all users", (done) => {
              chai.request(server)
                  .get('/users/findAllUsers')
