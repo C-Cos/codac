3e81f17b7b1b5db59adb91b4356933d1f3be49cb
Céline Cossez <celine.cossez@coding-academy.fr>
2019-06-03 19:09:00 +0200

    Comment section working + design

---
+++ frontend/src/components/Comments/DisplayComents.js
@@ -0,0 +1,129 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './Comment.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'
+
+
+export default class DisplayComments extends Component{
+
+    constructor(props){
+        super(props);
+        this.Delete = this.Delete.bind(this);
+        this.Edit = this.Edit.bind(this);
+        this.onChangeNewComment = this.onChangeNewComment.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            show: false,
+            newComment: this.props.obj.description,
+            date: '',
+            hour: ''
+        }
+    }
+    onChangeNewComment(e){
+        this.setState({
+            newComment: e.target.value
+        })
+    }
+    Delete(e){
+        e.preventDefault();
+        if(window.confirm("Etes-vous sûr de vouloir supprimer votre commentaire ?"))
+        {
+            axios.delete('http://localhost:4242/comment', {data : {id: this.props.obj._id}})
+            .then(response => {
+                this.props.delete(this.props.var);
+            })
+            .catch(function(err){
+                console.log(err);
+            })
+        }
+    }
+    Edit(e){
+        e.preventDefault();
+        this.setState({ show: true });
+    }
+    componentDidMount(){
+        var first = JSON.stringify(this.props.obj.created_date);
+        var sec =  first.split("T");
+        console.log(sec[0]);
+        var time = sec[1];
+        var thr = time.split('.');
+        console.log(thr[0]);
+        this.setState({
+            date: sec[0],
+            hour: thr[0]
+        })
+    }
+    hideModal = () => {
+        this.setState({ show: false });
+    };
+    onSubmit(e){
+        e.preventDefault();
+        axios.put('http://localhost:4242/comment', {data : {id: this.props.obj._id, description: this.state.newComment}})
+            .then(response => {
+                console.log("ok");
+                this.props.update(this.props.var, this.state.newComment);
+            })
+            .catch(function(err){
+                console.log(err);
+            })
+        this.setState({ show: false });
+    }
+
+
+
+    render() {
+        return(
+            <div className="container" style={{marginBottom: "30px"}}>
+                <Modal show={this.state.show} handleClose={this.hideModal}>
+                    <form onSubmit={this.onSubmit} style={{paddingLeft: 100}}>
+                        <label>Modifier votre commentaire :</label>
+                        <br/>
+                        <div className="form-group">
+                            <textarea rows = "5" cols = "50" name = "NewComment" value={this.state.newComment} onChange={this.onChangeNewComment}> </textarea>
+                        </div>
+                        <br/>
+                        <div className="form-group">
+                            <input id="commentSubmit" type="submit" value="Modifier"/>
+                        </div>
+                    </form>
+                </Modal>
+                <hr />
+                <div className="row">
+                    <div className="col">
+                        De : {this.props.obj.username}
+                        <br/>
+                        Posté le : {this.state.date}
+                        <br/>
+                        A : {this.state.hour}
+                    </div>
+                    <div className="col-9">
+                        "{this.props.obj.description}"
+                    </div>
+                    <div className="col">
+                        <button onClick = {this.Delete} style={{backgroundColor: "transparent", border: "none"}}>
+                            <FontAwesomeIcon icon={faTrashAlt} style={{fontSize:"20px", color:"black", margin:"10px"}}/>
+                        </button>
+                        <button onClick = {this.Edit} style={{backgroundColor: "transparent", border: "none"}}>
+                            <FontAwesomeIcon icon={faEdit} style={{fontSize:"20px", color:"black", margin:"10px"}}/>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const Modal = ({ handleClose, show, children }) => {
+    const showHideClassName = show ? "modal display-block" : "modal display-none";
+  
+    return (
+      <div className={showHideClassName}>
+        <section className="modal-main">
+            <button type="button" className="btn btn-outline-dark" onClick={handleClose}>close</button>
+            {children}
+        </section>
+      </div>
+    );
+};
---
+++ frontend/src/components/Comments/tempcomment.js
@@ -0,0 +1,119 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+import TableComment from "./DisplayComents";
+
+export default class Comments extends Component{
+
+    constructor(props){
+        
+        super(props);
+        this.onChangeComment = this.onChangeComment.bind(this);
+        this.updateList = this.updateList.bind(this);
+        this.dynamicList = this.dynamicList.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            username:'',
+            result: [],
+            comment: "",
+        };
+
+    }
+
+    onChangeComment(e) {
+        this.setState({
+            comment: e.target.value
+        });
+    }
+
+    componentDidMount(){
+        const token = localStorage.usertoken;
+        if(!token){
+            this.props.history.push('/login')
+        }
+        else {
+            const decoded = jwt_decode(token);
+            this.setState({
+                username : decoded.username,
+            })
+        }
+        axios.get('http://localhost:4242/comment', {params : {idEvent: "5cf165edd19835214b4658dd"}})  //this.props.match.params.id
+        .then(response => {
+            //console.log(response.data);
+            this.setState({
+                result: response.data
+            })
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+    }
+
+    onSubmit(){
+        var params = {
+            idEvent: "5cf165edd19835214b4658dd",  //this.props.match.params.id
+            username: this.state.username,
+            description: this.state.comment
+        }
+        axios.post('http://localhost:4242/comment', params)
+        .then(response => {
+            console.log("added ok");
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+    }
+
+    tab(){
+        let self = this;
+        return this.state.result.map(function(object, i){
+            return <TableComment obj={object} key={i} var={i} delete={self.updateList} update={self.dynamicList}/>;
+        });
+    }
+
+    updateList(key){
+        //console.log(this.state.result)
+        var array = [...this.state.result]; // make a separate copy of the arra
+        if (key !== -1) {
+          array.splice(key, 1);
+          this.setState({result: array});
+        } 
+    }
+
+    dynamicList(key, value){
+        var array = [...this.state.result]
+        array[key].description = value 
+        //console.log(array[key]);
+        this.setState({
+            result: array
+        })
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                    </div>
+                        <div className="col">
+                            <form onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <label>Entrer un commentaire : </label>
+                                    <br/>
+                                    <textarea rows = "5" cols = "70" name = "comment" value={this.state.comment} onChange={this.onChangeComment} required> </textarea>
+                                    <br/>
+                                </div>   
+                                <div className="form-group">
+                                    <input id="commentSubmit" type="submit" value="Ajouter" className="btn btn-dark"/>
+                                </div>
+                            </form>
+                        </div>
+                    <div className="col">
+                    </div>
+                    {this.tab()}
+                </div>
+            </div>
+        )
+    }
+}
